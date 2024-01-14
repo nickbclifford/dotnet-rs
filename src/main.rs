df10aea3b7b2a11a4c41553adff2364c08f5e6c3
Nick Clifford <nick@nickclifford.me>
2024-01-13 19:48:43 -0600

    start resolution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use dotnetdll::prelude::*;
+use std::{env::args, fs::File, io::prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = args().collect();
+    let input_filename = args
+        .get(1)
+        .expect("missing input filename (TODO: usage instructions)");
+    let mut input_file =
+        File::open(input_filename).expect(&format!("could not open file {}", input_filename));
+    let mut input_buf = vec![];
+    input_file
+        .read_to_end(&mut input_buf)
+        .expect("failed to read input file");
+    let resolution = Resolution::parse(&input_buf, ReadOptions::default())
+        .expect("failed to parse input file as .NET metadata");
+    println!("{:#?}", resolution)
 }
